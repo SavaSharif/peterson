363f8331c6b1fa42b34d9a5f65d404e1a67a2663
Sava <savasharif@gmail.com>
2022-03-04 15:55:59 +0100

    Added pseudocode using goto

---
+++ bla.py
@@ -0,0 +1,15 @@
+"""
+0:  if level[i] < N - 1:
+1:      last[level[i]] := i;
+2:      if k < N:
+3:         if k==i: 
+4:              goto 2; 
+5:         await last[level[i]]!=i or level[k] < level[i];
+6:         k++;
+7:         goto 2;
+8:      level[i]++;
+9:      goto 0;
+10:  nop;
+11:  level[i] := 0;
+12:  goto 0;
+"""
